be58ab84da406c3d96c19cb8c734b6a93a67f24d
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-11-25 20:56:42 -0600

    new game is now activated by space bar

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,10 @@
+import "babel-polyfill";
 import './index.scss'
 
 import { render } from 'react-dom';
 import React from 'react';
 
-import Hangman from './gameLogic/game.js'
+import Hangman from '../gameLogic/game.js'
 import {Score, Word, HangmanImage, WrongLetters} from './components/index.js'
 
 class App extends React.Component {
@@ -12,16 +13,25 @@ class App extends React.Component {
     state = {
       blankWord: '',
       wrongLetters: [],
-      lives: 0
-
+      playing: true
     }
 
 
 handleKeyPress = (event) => {
-
     let guess = event.key;
 
+    if(event.code === "Space" && !this.state.playing){
 
+      this.game.startNewRound()
+      this.setState({blankWord: this.game.blankVisibles})
+      this.setState({lives: this.game.lives})
+
+      let wrongArr = [...this.game.wrongLetters.values()]
+      this.setState({wrongLetters: wrongArr})
+      this.setState({playing: true})
+    }
+
+if(this.state.playing){
     if (/^[a-zA-Z]{1}$/.test(guess)){
           this.game.checkForLetter(event.key)
           this.setState({blankWord: this.game.blankVisibles})
@@ -32,24 +42,36 @@ handleKeyPress = (event) => {
           this.setState({wrongLetters: wrongArr})
 
           this.setState({lives: this.game.lives})
-          this.setState({gameOver: this.game.gameOver})
+          // this.setState({gameOver: this.game.gameOver})
           this.setState({wins: this.game.wins})
           this.setState({losses: this.game.losses})
     }
-    // else{
-    //   console.log(`${guess} is not a letter`);
-    // }
+  }
+
 
   }
 
+
+componentDidUpdate(prevProps, prevState){
+  if(prevState.wins != this.state.wins){
+    this.setState({playing: false})
+  }
+
+  if(prevState.losses != this.state.losses){
+    this.setState({playing: false})
+  }
+}
+
+
 componentWillMount() {
     this.game.initializeGame()
-    console.log(this.game);
     this.setState({blankWord: this.game.blankVisibles})
     this.setState({lives: this.game.lives})
     this.setState({gameOver: this.game.gameOver})
     this.setState({wins: this.game.wins})
     this.setState({losses: this.game.losses})
+
+
   }
 
 componentDidMount() {
@@ -59,7 +81,7 @@ componentDidMount() {
   render() {
         return (
             <div className='App'>
-              <Word word={this.state.blankWord}/>
+              <Word playing={this.state.playing} word={this.state.blankWord}/>
               <WrongLetters wrongLetters={this.state.wrongLetters}/>
               <HangmanImage lives={this.state.lives}/>
               <Score wins={this.state.wins} losses={this.state.losses}/>
